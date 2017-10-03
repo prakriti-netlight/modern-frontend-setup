78f4554287487179133872d9d568913e01ab3f92
prakriti-netlight <prti@netlight.com>
2017-10-03 15:07:52 +0200

    added react

--- src/app.js
+++ src/app.js
@@ -1,2 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const root = document.getElementById('root');
-root.innerHTML = '<p>Hello Webpack</p>';
+
+const nextWord =  '<h1>Welcome to Codepub</h1>';
+
+//Old way to write a function
+function oldWay () {
+    return '<h1>Hello Webpack</h1>' + nextWord
+}
+
+//Arrow function
+const newWay = () => `<h1>Hello Webpack</h1>${nextWord}`;
+
+//React Component
+const reactComponent = (
+   <h1>Hello Codepub</h1>
+);
+
+class ReactListComponents extends React.Component {
+    render() {
+        return [
+            <h1 key="1">Hello Codepub</h1>,
+            <h2 key="2">Hello React!</h2>
+        ];
+    }
+}
+//DOM
+//root.innerHTML = newWay();
+
+//React with rendering function
+//ReactDOM.render(<ReactListComponents />, root);
+
+//React with rendering components that return a list
+ReactDOM.render(<ReactListComponents />, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
 
 const config = {
-   context: path.resolve(__dirname, 'src'),
-   entry: './app.js',
-   output: {
-       path: path.resolve(__dirname, 'dist'),
-       filename: 'bundle.js'
-   }
-}
+    'context': path.resolve(__dirname, 'src'),
+    'entry': './app.js',
+    'output': {
+        'path': path.resolve(__dirname, 'dist'),
+        'filename': 'bundle.js'
+    },
+    'module': {
+        'rules': [
+            {
+                'test': /\.js|.jsx?$/,
+                'include': path.resolve(__dirname, 'src'),
+                'exclude': [/node_modules/],
+                'use': [{'loader': 'babel-loader'}]
+            }
+        ]
+    }
+};
 
 module.exports = config;
